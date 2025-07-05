b6baf6a1844c098cd530693138cf36d60ca7ca76
adiodnaoda <122939450+adiodnaoda@users.noreply.github.com>
2025-07-05 04:36:56 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-module.exports={
-    probot_ids :'282859044593598464',//أيدي بروبوت
-    recipientId : '1352053059466952828',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
-    price : '20000 ',//سعر النسخ
-    serverid : '1385876085182562395',//أيدي سيرفارك
-    token : 'MTM5MDc0OTk2NTk2MTEzNDIwMw.GmP8pP.PUzws8XnflT19B9P0g7fTV05qSLKMJvv6nHuJ8'//توكن بوتك
-}
+
